e3bce3b2c91e3d849e65d076dd5086dcd489ea36
Nalain7t2 <abbas7t2@gmail.com>
2026-02-17 20:03:41 +0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-default-dev-ke
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', '')
 
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
