7fb37ce97f66dfc417e045fe6651aec4358a6f30
Nalain7t2 <abbas7t2@gmail.com>
2026-02-17 20:01:17 +0500

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -25,8 +25,12 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG or not settings.DEBUG:  
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
     )
